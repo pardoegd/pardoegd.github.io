b45ef9c9406584b12776aa33fafae7395303369d
pardoenator <daniel.pardoe@hotmail.com>
2018-06-21 16:28:02 +0100

    Footer Styling and Gallery Prototyping
    Footer Styling and Gallery Prototyping.........

--- Image-Gallery.js
+++ Image-Gallery.js
@@ -1,13 +1,3 @@
-// Open the Modal
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
-
-// Close the Modal
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
-
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -23,18 +13,16 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
+  var slides = document.getElementsByClassName("section.mySlides");
+  var dots = document.getElementsByClassName("section.dot");
+  if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+      slides[i].style.display = "none"; 
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
 }
