97b0ff89ff8ed71bc92e935b9aa4c59d1804fe2b
pardoenator <daniel.pardoe@hotmail.com>
2018-06-29 21:54:22 +0100

    Styling Changes, Basic Gallery Implimented  and More Pages Added
    Styling Changes, Basic Gallery Implimented  and More Pages Added.......

--- Slideshows.js
+++ Slideshows.js
@@ -1,28 +1,8 @@
-var slideIndex = 1;
-showDivs(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showDivs(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
-  }
-  x[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " w3-opacity-off";
-}
+$(function() {
+    $(".rslides").responsiveSlides({
+        auto: false,
+        random: false,
+        nav: true,
+        pager: true,
+    });
+});
